7a9aea44000fc981d849997aca1ec9508e609b67
wilson29thid <wilson@29th.org>
2015-02-04 07:28:50 -0500

    Working - move staging to prod

--- deploy.php
+++ deploy.php
@@ -1,7 +1,20 @@
 <?php
-$working_dir = getenv('WORKING_DIR') ?: '../personnel-app/';
-$staging_dir = '../staging-personnel-app/';
-$update_script = './update.sh';
+// This is working! Just need to move the below to env vars
+$config = array(
+    'personnel-app' => array(
+        'production_dir' => '/var/www/html/personnel-app/',
+        'staging_dir' => '/var/www/html/staging-personnel-app/',
+        'backup_dir' => '/var/www/html/backup-personnel-app/',
+        'update_script' => './update.sh'
+    ),
+    'personnel-api' => array(
+        'production_dir' => '/var/www/html/personnel-api/',
+        'staging_dir' => '/var/www/html/staging-personnel-api/',
+        'backup_dir' => '/var/www/html/backup-personnel-api/',
+        'update_script' => './update.sh'
+    )
+);
+$paths = sizeof($argv) > 1 ? $config[$argv[1]] : $config['personnel-app'];
 
 // Verify request is authentic (from GitHub)
 /*$headers = getallheaders();
@@ -17,9 +30,16 @@ if($hash !== $payloadHash) {
 }*/
 
 // Create staging directory
-echo shell_exec('set -x ; rm -rf ' . $staging_dir);
-echo shell_exec('set -x ; cp -R ' . $working_dir . ' ' . $staging_dir);
-chdir($staging_dir);
+echo shell_exec('set -x ; rm -rf ' . $paths['staging_dir']);
+echo shell_exec('set -x ; cp -R ' . $paths['production_dir'] . ' ' . $paths['staging_dir']);
 
-// Execute update script
-echo shell_exec('bash ' . $update_script);
+// Execute update script inside staging directory
+chdir($paths['staging_dir']);
+echo shell_exec('bash ' . $paths['update_script']);
+
+// Backup previous build
+echo shell_exec('set -x ; rm -rf ' . $paths['backup_dir']);
+echo shell_exec('set -x ; mv ' . $paths['production_dir'] . ' ' . $paths['backup_dir']);
+
+// Move staging to production
+echo shell_exec('set -x ; mv ' . $paths['staging_dir'] . ' ' . $paths['production_dir']);
